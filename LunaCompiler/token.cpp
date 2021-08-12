1f39562789039d7ac5d68e9f909c286f226062a4
Dplepler <david.plepler@gmail.com>
2021-08-12 15:13:51 +0300

    Deleted cpp file

--- LunaCompiler/token.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "token.h"
-
-token_T* init_token(int type, char* value)
-{
-	token_T* token = (token_T*)calloc(1, sizeof(token_T));
-	*token = { type, value };
-}
